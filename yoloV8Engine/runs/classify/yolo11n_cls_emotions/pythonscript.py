aec1517e5e320c76651300bc8f99318173f12540
lmoboy <steamgoyu19@gmail.com>
2024-11-11 15:26:58 +0200

    updated to use deepface for detection (i spent too much time trying to make a better model in yolo 😭😭😭)

--- yoloV8Engine/runs/classify/yolo11n_cls_emotions/pythonscript.py
+++ yoloV8Engine/runs/classify/yolo11n_cls_emotions/pythonscript.py
@@ -1,16 +1,24 @@
 from ultralytics import YOLO
+from deepface import DeepFace
 from fastapi import FastAPI, File, UploadFile, HTTPException
-model = YOLO("C:\\laragon\\www\\Vibeify\\yoloV8Engine\\yolo11n-cls.pt", task="classify", verbose=True)
+model = YOLO("C:\\laragon\\www\\Vibeify\\yoloV8Engine\\newtrain.pt", task="classify", verbose=True)
+
 app = FastAPI()
 
-def run_picture(picture):
-    model_result = model.predict(source=picture, stream=False, imgsz=640, conf=0.5, save=False)
-    output = {}
-    for result in model_result:
-        keyed_names = result.names
-        keyed_probs = result.probs.top1
-        output = keyed_names[keyed_probs]
-    return output
+
+def run_picture_deepface(picture):
+    result = DeepFace.analyze(picture, actions=['emotion'], enforce_detection=False)
+    return result[0]["dominant_emotion"]
+
+# REST in API(eace) my beloved yolo model
+# def run_picture(picture):
+#     model_result = model.predict(source=picture, stream=False, imgsz=640, conf=0.7, save=False)
+#     output = {}
+#     for result in model_result:
+#         keyed_names = result.names
+#         keyed_probs = result.probs.top1
+#         output = keyed_names[keyed_probs]
+#     return output
 
 
 @app.post('/predict')
@@ -20,7 +28,7 @@ async def upload(file: UploadFile = File(...)):
         with open(f"storage/app/private/{file.filename}", 'wb') as f:
             while contents := file.file.read(1024 * 1024):
                 f.write(contents)
-            result = run_picture(f.name)
+            result = run_picture_deepface(f.name)
             
 
     except Exception as err:
@@ -29,6 +37,8 @@ async def upload(file: UploadFile = File(...)):
         file.file.close()
 
     return {'success':f"Successfully uploaded {file.filename}", 'result':str(result)}
+
+
 
 if __name__ == "__main__":
     import uvicorn
